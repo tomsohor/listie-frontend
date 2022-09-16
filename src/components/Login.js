c70e348c9ca573bc42c20eb86c9676ff30d4415d
tomsohor <tomsohor18@kit.edu.kh>
2022-09-16 16:56:23 +0700

    set up page router

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,19 +4,15 @@ function Login() {
     return ( 
 
         <div className="login">
-            <p className="tag">
-                Log in
-            </p>
-            <div className="content">
                 <h2>“ Listie, Your bestie to Reminisce your Products pricing! “ </h2>
                 <div className="form">
                     <input type="text" className="username" placeholder='username'/>
                     <input type="text" className="password" placeholder='Password'/>
                     <div className="action">
-                        <p>Don’t have Account? <a href=""> Create</a> Create</p>
+                        <p>Don’t have Account? <a href=""> Create</a></p>
                         <button className="loginbtn">Login</button>
                     </div>
-                </div>
+              
             </div>
         </div>
      );
