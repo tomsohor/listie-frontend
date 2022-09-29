1c914abfb495dcb4c718137285dd6a980215f6cf
tomsohor <tomsohor18@kit.edu.kh>
2022-09-29 11:55:24 +0700

    block item completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,9 +11,11 @@ import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [data, setData] = useState();
+  const [k, setK] = useState();
+
   const navigate = useNavigate();
   //filter by customer type
-  const [filter,setFilter] = useState({state:false,type:''});
+  const [filter, setFilter] = useState({ state: false, type: "" });
   const toDelete = (id) => {
     navigate("/item/delete", { state: { id } });
   };
@@ -32,41 +34,53 @@ function Home() {
     });
   };
 
-  const onCustomerType = async(type) =>{
-    if(type === 'x'){
-        setFilter(f => ({
-            ...f,
-            state:false,
-            type:''
-        }))
-        await getAllData();
+  const onCustomerType = async (type) => {
+    if (type === "x") {
+      setFilter((f) => ({
+        ...f,
+        state: false,
+        type: "",
+      }));
+      await getAllData();
     }
-    if(type === 'retailer'){
-        setFilter(f => ({
-            ...f,
-            state:true,
-            type:'retailer'
-        }))
-        const retailers = data.filter(item => item.customertype === 'retailer')
-        setData(retailers)
+    if (type === "retailer") {
+      setFilter((f) => ({
+        ...f,
+        state: true,
+        type: "retailer",
+      }));
+      const retailers = data.filter((item) => item.customertype === "retailer");
+      setData(retailers);
     }
-    if(type === 'end-consumer'){
-        setFilter(f => ({
-            ...f,
-            state:true,
-            type:'end-consumer'
-        }))
-        const endUser = data.filter(item => item.customertype === 'end-consumer')
-        setData(endUser)
+    if (type === "end-consumer") {
+      setFilter((f) => ({
+        ...f,
+        state: true,
+        type: "end-consumer",
+      }));
+      const endUser = data.filter(
+        (item) => item.customertype === "end-consumer"
+      );
+      setData(endUser);
+    }
+  };
 
-    }
-  }
-
-  const getAllData = async() => {
+  const getAllData = async () => {
     const dataset = await api.getAllData();
-      setData(dataset.data);
-      
-  }
+
+    const tmp = {};
+    dataset.data.forEach((item) => {
+      if (!tmp[item.itemtype]) {
+        tmp[item.itemtype] = [item];
+      } else {
+        tmp[item.itemtype].push(item);
+      }
+    });
+
+    setData(dataset.data);
+    setK(tmp);
+    console.log(tmp);
+  };
 
   useEffect(() => {
     getAllData();
@@ -78,11 +92,28 @@ function Home() {
           <Search />
           <div className="ctype_container">
             <div className="customertype">
-            { filter.state === true && <span className="x" onClick={()=>onCustomerType('x')}>X</span>}
-              {filter.type !== 'end-consumer' && <span className="retailer" onClick={()=>onCustomerType('retailer')}>Retailer</span>}
-              
-              { filter.type !== 'retailer' && <span className="end-consumer" onClick={()=>onCustomerType('end-consumer')}>End Consumer</span>}
-              
+              {filter.state === true && (
+                <span className="x" onClick={() => onCustomerType("x")}>
+                  X
+                </span>
+              )}
+              {filter.type !== "end-consumer" && (
+                <span
+                  className="retailer"
+                  onClick={() => onCustomerType("retailer")}
+                >
+                  Retailer
+                </span>
+              )}
+
+              {filter.type !== "retailer" && (
+                <span
+                  className="end-consumer"
+                  onClick={() => onCustomerType("end-consumer")}
+                >
+                  End Consumer
+                </span>
+              )}
             </div>
           </div>
 
@@ -91,46 +122,48 @@ function Home() {
               <h2>Grand Total</h2>
               <p>{data.length} items</p>
             </div>
-            
-            <div className="itemtype">
-              <div className="typename">
-                <div className="name">
-                  <h3>Drink</h3>
-                  <p>150 items</p>
-                </div>
-                <MdOutlineOtherHouses className="more" />
-              </div>
-              <div className="items">
-                {data.map((item) => (
-                  <div className="item" key={item.id}>
-                    <span className={"tag " + item.customertype}></span>
 
-                    <span className="name">
-                      <span className="nametooltip">{item.itemname}</span>
-                      <span onClick={() => getDetails(item.id)}>
-                        {item.itemname}
-                      </span>
-                    </span>
-                    <div className="img">
-                      <img
-                        // src={"../image/coca.jpg" }
-                        src={coca}
-                        alt=""
-                      />
-                    </div>
-
-                    <div className="action">
-                      <IoDuplicateOutline onClick={() => toDuplicate(item)} />
-                      <RiDeleteBin6Line onClick={() => toDelete(item.id)} />
-                    </div>
+            {Object.keys(k).map(itemtype => (
+              <div className="itemtype">
+                <div className="typename">
+                  <div className="name">
+                    <h3>{itemtype}</h3>
+                    <p>{k[itemtype].length} items</p>
                   </div>
-                ))}
-              </div>
-              {/* <div className="paging">
+                  <MdOutlineOtherHouses className="more" />
+                </div>
+                <div className="items">
+                  {k[itemtype].map((item) => (
+                    <div className="item" key={item.id}>
+                      <span className={"tag " + item.customertype}></span>
+
+                      <span className="name">
+                        <span className="nametooltip">{item.itemname}</span>
+                        <span onClick={() => getDetails(item.id)}>
+                          {item.itemname}
+                        </span>
+                      </span>
+                      <div className="img">
+                        <img
+                          // src={"../image/coca.jpg" }
+                          src={coca}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="action">
+                        <IoDuplicateOutline onClick={() => toDuplicate(item)} />
+                        <RiDeleteBin6Line onClick={() => toDelete(item.id)} />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                {/* <div className="paging">
                         <span className="pre"><GrPrevious/></span>
                         <span className="next"><GrNext/></span>
                     </div> */}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
